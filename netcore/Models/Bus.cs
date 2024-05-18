502e73ed92f17174695951c300b7611ba7e475c4
Edalat Feizi <edalatfeizi921@gmail.com>
2024-05-18 22:19:27 +0200

    add unit tests to test CongestionTaxCalculator class

---
+++ netcore/Models/Bus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace congestion.calculator.Models
+{
+    public class Bus : Vehicle
+    {
+        public string GetVehicleType()
+        {
+            return "Bus";
+        }
+    }
+}
---
+++ netcore/Models/Tractor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace congestion.calculator.Models
+{
+    public class Tractor : Vehicle
+    {
+        public string GetVehicleType()
+        {
+            return "Tractor";
+        }
+    }
+}
